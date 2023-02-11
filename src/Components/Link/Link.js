6f52e18789a852f21ccf9311bfe7b6fd96915089
Yassine Boukhla <yassineboukhla2018@gmail.com>
2023-02-11 23:18:40 +0100

    Desktop version finished

--- src/Components/Link/Link.js
+++ src/Components/Link/Link.js
@@ -1,12 +1,19 @@
 import React from "react";
 import styles from "./Link.module.css";
 function Link({ oldUrl, shortUrl }) {
+  const copyHandler = (event) => {
+    navigator.clipboard.writeText(shortUrl);
+    event.target.innerText = "Copied!";
+    setTimeout(() => {
+      event.target.innerText = "Copy";
+    }, 5000);
+  };
   return (
     <div className={styles.link}>
       <p>{oldUrl}</p>
       <div className={styles.link_wrapper}>
         <a href={shortUrl}>{shortUrl}</a>
-        <button>Copy</button>
+        <button onClick={copyHandler}>Copy</button>
       </div>
     </div>
   );
--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -15,23 +15,30 @@ function Statistics() {
         </p>
       </div>
       <div className={styles.statistics_cards}>
-        <Card
-          url={brandIMG}
-          title="Brand Recognition"
-          description="Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instill confidence in your content."
-        />
-        <Card
-          url={recordIMG}
-          title="Detailed Records"
-          description=" Gain insights into who is clicking your links. Knowing when and where 
-          people engage with your content helps inform better decisions."
-        />
-        <Card
-          url={customIMG}
-          title="Fully Customizable"
-          description="Improve brand awareness and content discoverability through customizable 
-          links, supercharging audience engagement."
-        />
+        <div>
+          <Card
+            url={brandIMG}
+            title="Brand Recognition"
+            description="Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instill confidence in your content."
+          />
+        </div>
+
+        <div style={{ marginTop: "60px" }}>
+          <Card
+            url={recordIMG}
+            title="Detailed Records"
+            description=" Gain insights into who is clicking your links. Knowing when and where 
+        people engage with your content helps inform better decisions."
+          />
+        </div>
+        <div style={{ marginTop: "120px" }}>
+          <Card
+            url={customIMG}
+            title="Fully Customizable"
+            description="Improve brand awareness and content discoverability through customizable 
+links, supercharging audience engagement."
+          />
+        </div>
       </div>
     </div>
   );
