0a6786ad90ca330cf14b6de25d2068b3184413d0
Yassine Boukhla <yassineboukhla2018@gmail.com>
2023-02-13 21:37:01 +0100

    mobile menu functional

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,23 +1,45 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styles from "./Header.module.css";
 import logo from "./images/logo.svg";
-import { HiOutlineMenu } from "react-icons/hi";
+import { HiOutlineMenu, HiX } from "react-icons/hi";
 function Header() {
+  const [closer, setCloser] = useState(false);
+  const headerMobileRef = useRef();
+  const menuOpenHandler = (e) => {
+    headerMobileRef.current.classList.add(styles.visible);
+    setCloser(true);
+  };
+  const menuCloseHandler = (e) => {
+    headerMobileRef.current.classList.remove(styles.visible);
+    setCloser(false);
+  };
   return (
     <div className={styles.header}>
       <div className={styles.header_wrap}>
         <img src={logo} alt="" />
-        <ul className={styles.header_list}>
+        <ul className={styles.header_list} ref={headerMobileRef}>
           <li>Features</li>
           <li>Pricing</li>
           <li>Resources</li>
+          <div className={styles.header_auth_mobile}>
+            <button className={styles.btn_login}>Login</button>
+            <button className={styles.btn_signup}>Sign Up</button>
+          </div>
         </ul>
       </div>
       <div className={styles.header_auth}>
         <button className={styles.btn_login}>Login</button>
         <button className={styles.btn_signup}>Sign Up</button>
       </div>
-      <HiOutlineMenu className={styles.header_menu_icon} />
+      {closer === false && (
+        <HiOutlineMenu
+          className={styles.header_menu_icon}
+          onClick={menuOpenHandler}
+        />
+      )}
+      {closer === true && (
+        <HiX className={styles.header_menu_close} onClick={menuCloseHandler} />
+      )}
     </div>
   );
 }
